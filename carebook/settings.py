bcd3a953b3916500eb17762ede244434ac3b2383
Ayse Kubra <aysekubrakupeli@gmail.com>
2018-02-02 16:32:20 +0000

    changed allowed hosts

--- carebook/settings.py
+++ carebook/settings.py
@@ -29,7 +29,7 @@ USE_S3 = os.environ.get("USE_S3", True)
 
 ALLOWED_HOSTS = [
     'stream-3-project-aysekubrakupeli.c9users.io',
-    'https://aysekubrakupeli-carebook.herokuapp.com/',
+    'aysekubrakupeli-carebook.herokuapp.com/'
 ]
 
 # Application definition
